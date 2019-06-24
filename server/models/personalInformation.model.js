b87ef1aa6aa32c7342c6f9457c0748d53a76ee03
Enrique Palacios <enrique.palacios@struck.com>
2019-06-24 17:43:18 -0600

    Add array properties

--- server/models/personalInformation.model.js
+++ server/models/personalInformation.model.js
@@ -10,6 +10,10 @@ const personalInformation = new Schema({
   email: { type: String, required: true },
   phoneNumber: String,
   address: String,
+  skills: Array,  // example  { name: "VueJs", value: 40 }
+  languages: Array, // example  { name: "English", value: 75 }
+  workExperience: Array, // { title: "Frontend Developer / Struck - NicaSource",dateRange: "Apr 2019 - current", description: "Vue.js, Ruby On Rails 3 and AngularJS websites development."}
+  education: Array // { title: "Android ATC / www.androidatc.com", dateRange: "August 25, 2018", description: "AND-801	- Android Certified Application Developer v8"}
 });
 
 module.exports = mongoose.model('PersonalInformation', personalInformation);
