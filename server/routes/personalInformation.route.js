b1e800d6e84522a642b7d2d27fafec2c2fb95613
Enrique Palacios <enrique.palacios@struck.com>
2019-06-27 17:41:54 -0600

    Implement deletion

--- server/routes/personalInformation.route.js
+++ server/routes/personalInformation.route.js
@@ -1,37 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const PersonalInformation = require('../models/personalInformation.model');
+const PersonalInformation = require("../models/personalInformation.model");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   PersonalInformation.find({}, (error, data) => {
     if (error) return console.log(error);
     res.send(data);
-  })
+  });
 });
 
-router.get('/:id', (req, res) => {
-  PersonalInformation.find({_id: req.params.id}, (error, data) => {
+router.get("/:id", (req, res) => {
+  PersonalInformation.find({ _id: req.params.id }, (error, data) => {
     if (error) return console.log(error);
     console.log(data);
     res.send(data);
-  })
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newRecord = new PersonalInformation(req.body);
   newRecord.save((error, data) => {
-    if (error) return console.log('there was an error inserting the object ', error);
+    if (error)
+      return console.log("there was an error inserting the object ", error);
     res.send(data);
-    console.log('record saved! ', data);
-  })
-})
+    console.log("record saved! ", data);
+  });
+});
 
-router.put('/:id', (req, res) => {
-  res.send('this will update a basic profile');
-})
+router.put("/:id", (req, res) => {
+  res.send("this will update a basic profile");
+});
 
-router.delete('/:id', (req, res) => {
-  res.send('this will delete a specific profile');
-})
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  PersonalInformation.findByIdAndRemove({ _id: id }, (error, data) => {
+    if (error) {
+      res.send("An error occurred :(");
+    } else {
+      res.send(`Registry with id ${id} removed`);
+    }
+  });
+});
 
 module.exports = router;
