71054de75c4337134140141967525936ad646223
Enrique Palacios <enrique.palacios@struck.com>
2019-07-10 13:02:15 -0600

    Enhance name and photo display styling

--- app/src/components/ProfileDetailMainInfo.vue
+++ app/src/components/ProfileDetailMainInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="w3-display-container avatar-container">
-      <img :src="emp.profilePicture" style="width: 100%;" alt="Avatar">
-      <div class="w3-display-bottomleft w3-container w3-text-white">
+      <img class="photo" :src="emp.profilePicture" alt="Avatar" />
+      <div class="name w3-display-bottomleft w3-container w3-text-white">
         <h2>{{`${emp.firstName} ${emp.lastName}`}}</h2>
       </div>
     </div>
@@ -24,7 +24,7 @@
         <i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>
         {{emp.phoneNumber}}
       </p>
-      <hr>
+      <hr />
     </div>
   </div>
 </template>
@@ -43,7 +43,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+.photo {
+  width: 100%;  
+}
+
+.name{
+  z-index: 2;
+}
+
+.avatar-container {
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    display: inline-block;
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.6)
+    );
+  }
+}
 </style>
 
 
