30d2069438d139f6c49e7ce3c4bee5c19c2f1322
Enrique Palacios <enrique.palacios@struck.com>
2019-10-03 14:37:29 -0600

    Discard webpack manually config

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,16 +1,6 @@
 var webpack = require('webpack')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'VUE_APP_PROFILES_API_BASE_URL': JSON.stringify(process.env.VUE_APP_PROFILES_API_BASE_URL),
-          'VUE_APP_DEFAULT_PROFILE_ID': JSON.stringify(process.env.VUE_APP_DEFAULT_PROFILE_ID)
-        }
-      })
-    ]
-  },
   css: {
     loaderOptions: {
       sass: {
