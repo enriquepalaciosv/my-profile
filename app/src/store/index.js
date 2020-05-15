cb8997286d92175139a08f4653e191dd4024b9db
Enrique Palacios <enrique.palacios@struck.com>
2020-05-15 14:33:58 -0600

    Fetch data from S3

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,47 +1,49 @@
 import axios from "axios";
-import data from './data';
 
 export default {
-    state: {
-        employees: [],
-        editMode: false
+  state: {
+    employees: [],
+    editMode: false
+  },
+  getters: {
+    findEmployeeById: state => id => {
+      return state.employees.find(emp => emp["_id"] == id);
     },
-    getters: {
-        findEmployeeById: state => id => {
-            return data;
-        },
-        editMode: state => state.editMode
+    editMode: state => state.editMode
+  },
+  actions: {
+    fetchEmployees: ({ commit }) => {
+      axios
+        .get(
+          "https://profile-app-pictures.s3.us-east-2.amazonaws.com/data.json"
+        )
+        .then(response => commit("setEmployees", [response.data]));
     },
-    actions: {
-        fetchEmployees: ({commit}) => {    
-            commit("setEmployees", [data]);            
-        },
-        activateEditMode: ({commit}) => {
-            commit("setEditMode", true);
-        },
-        deactivateEditMode: ({commit}) => {
-            commit("setEditMode", false);
-        },
-        updateEmployee: ({commit}, {id, profile}) => {
-            axios
-                .put(`/profiles/${id}`, profile)
-                .then(response => {
-                    alert("Registro actualizado");
-                    commit("setEditMode", false);
-                    commit("updateEmployee", response.data);
-                });
-
-        }
+    activateEditMode: ({ commit }) => {
+      commit("setEditMode", true);
     },
-    mutations: {
-        setEmployees: (state, employees) => {
-            state.employees = employees;
-        },
-        setEditMode: (state, mode) => {
-            state.editMode = mode;
-        },
-        updateEmployee: (state, updated) => {
-            state.employees = state.employees.map(emp => emp["_id"] === updated["_id"] ? updated : emp);
-        }
+    deactivateEditMode: ({ commit }) => {
+      commit("setEditMode", false);
+    },
+    updateEmployee: ({ commit }, { id, profile }) => {
+      axios.put(`/profiles/${id}`, profile).then(response => {
+        alert("Registro actualizado");
+        commit("setEditMode", false);
+        commit("updateEmployee", response.data);
+      });
     }
+  },
+  mutations: {
+    setEmployees: (state, employees) => {
+      state.employees = employees;
+    },
+    setEditMode: (state, mode) => {
+      state.editMode = mode;
+    },
+    updateEmployee: (state, updated) => {
+      state.employees = state.employees.map(emp =>
+        emp["_id"] === updated["_id"] ? updated : emp
+      );
+    }
+  }
 };
